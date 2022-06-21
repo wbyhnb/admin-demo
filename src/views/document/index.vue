ed0948a87888ce735f5333c18986225bef44a9e7
BIYUWEN <wbyhnb1314@163.com>
2022-06-21 18:30:20 +0800

    菜单。路由，导航配置

--- src/views/document/index.vue
+++ src/views/document/index.vue
@@ -1,10 +1,19 @@
 <template>
-  <div class="">文档</div>
+  <div class="">文档
+  <MarkdownItVue class="md-body" :content="content" /></div>
 </template>
 
 <script>
+import MarkdownItVue from 'markdown-it-vue'
+import readme from "./../../.././README.md";
+import 'markdown-it-vue/dist/markdown-it-vue.css'
 export default {
-
+components:{MarkdownItVue},
+data() {
+    return {
+      content:readme,
+    }
+  },
 }
 </script>
 
