f2f2714f7ea05978cd45c047be0ed4eb0c37cc38
BIYUWEN <wbyhnb1314@163.com>
2022-07-04 18:33:25 +0800

    定时器

--- src/views/document/index.vue
+++ src/views/document/index.vue
@@ -9,7 +9,7 @@
   <el-button type="danger">危险按钮</el-button>
 </el-row>
 <el-input v-model="input" placeholder="请输入内容"></el-input>
-
+{{times}}
 </div>
 </template>
 
@@ -19,6 +19,37 @@ export default {
 data() {
   return {
     input: '',
+    times:10
+  }
+},
+created() {
+  console.log('created');
+},
+mounted() {
+  console.log('mounted');
+  //60秒倒计时打印内容
+  this.timeLost()
+var a =10;
+   
+setTimeout(() => {
+  a--
+  console.log(a);
+}, 1000);
+
+},
+methods: {
+  timeLost() {
+   let timer = setInterval(() => {
+    this.times--;
+    if (this.times <= 0) {
+      clearInterval(timer);
+      // console.log('倒计时结束');
+      // this.times = 10;
+      // this.timeLost();
+    } else {
+      // console.log(this.times);
+    }
+  }, 1000);
   }
 },
 
